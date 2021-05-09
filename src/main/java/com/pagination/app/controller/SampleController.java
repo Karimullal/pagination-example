60f9314b54f91d9f37d310c18e4f844d361bb0f2
Karimullal <33246063+Karimullal@users.noreply.github.com>
2021-05-09 13:12:27 +0530

    Added sample controller for jenkins demo
    Added sample controller for jenkins demo

---
+++ src/main/java/com/pagination/app/controller/SampleController.java
@@ -0,0 +1,21 @@
+package com.pagination.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/sample")
+public class SampleController {
+
+	@GetMapping
+	private String sayHello(@RequestParam("name") String name) {
+    return "Hello "+name;
+	}
+	
+	
+}
